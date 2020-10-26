2d6c17475797c5122ec6e3047670396aafc1be15
Jorgen Verweij <jorgen@perplex.nl>
2020-10-26 11:55:00 +0100

    style.css toegevoegd + inline geplaatst

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-      return {
-        pathPrefix: "/_site/"
+    eleventyConfig.addFilter("cssmin", function(code) {
+      return new CleanCSS({}).minify(code).styles;
+    });
+
+    return {
+      pathPrefix: "/_site/"
     };
 };
